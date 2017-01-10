3f2b5043e83305d04ac73c13a4ccb36b94c35a94
fengbingchun <fengbingchun@163.com>
2017-01-10 09:49:50 +0800

    Add get layer list test code

--- demo/Caffe_Test/funset.cpp
+++ demo/Caffe_Test/funset.cpp
@@ -1,2 +1,20 @@
 #include "funset.hpp"
+#include "common.hpp"
+
+int get_layer_type_list()
+{
+	caffe::LayerRegistry<double>::CreatorRegistry& registry = caffe::LayerRegistry<double>::Registry();
+
+	std::vector<std::string> layers_list;
+	for (caffe::LayerRegistry<double>::CreatorRegistry::iterator iter = registry.begin(); iter != registry.end(); ++iter) {
+		layers_list.push_back(iter->first);
+	}
+
+	fprintf(stdout, "layer count: %d\n", layers_list.size());
+	for (int i = 0; i < layers_list.size(); i++) {
+		fprintf(stdout, "%d:    %s\n", i+1, layers_list[i].c_str());
+	}
+
+	return 0;
+}
 
--- demo/Caffe_Test/funset.hpp
+++ demo/Caffe_Test/funset.hpp
@@ -4,5 +4,6 @@
 int MNIST_train();
 int MNIST_predict();
 int MNIST_convert();
+int get_layer_type_list();
 
 #endif // FBC_CAFFE_TEST_FUNSET_HPP_
