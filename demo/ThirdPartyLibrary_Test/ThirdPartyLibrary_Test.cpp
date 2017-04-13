cd628ed5904b5a9385bfde59ed88b689c9192057
fengbingchun <fengbingchun@163.com>
2017-04-13 17:05:45 +0800

    Modify lmdb test code

--- demo/ThirdPartyLibrary_Test/ThirdPartyLibrary_Test.cpp
+++ demo/ThirdPartyLibrary_Test/ThirdPartyLibrary_Test.cpp
@@ -4,10 +4,11 @@
 #include "glog_test.hpp"
 #include "gflags_test.hpp"
 #include "leveldb_test.hpp"
+#include "lmdb_test.hpp"
 
 int main()
 {
-	int ret = test_leveldb_1();
+	int ret = test_lmdb_1();
 
 	if (ret == 0) fprintf(stderr, "=====  test success  =====\n");
 	else fprintf(stderr, "*****  test fail  *****\n");
---
+++ demo/ThirdPartyLibrary_Test/lmdb_test.hpp
@@ -0,0 +1,6 @@
+#ifndef FBC_CAFFE_TEST_LMDB_TEST_HPP_
+#define FBC_CAFFE_TEST_LMDB_TEST_HPP_
+
+int test_lmdb_1();
+
+#endif // FBC_CAFFE_TEST_LMDB_TEST_HPP_
