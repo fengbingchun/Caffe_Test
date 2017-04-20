c00cf6fb514a4df8782f44d84f1f9878112a0244
fengbingchun <fengbingchun@163.com>
2017-04-20 15:53:31 +0800

    Remove old third library files

--- src/thirdparty/HDF5/vs2013/c++/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/GitCode/Caffe_Test/src/thirdparty/HDF5/hdf5-1.8.15-patch1/c++/src
-# Build directory: E:/GitCode/Caffe_Test/src/thirdparty/HDF5/vs2013/c++/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- src/thirdparty/HDF5/vs2013/tools/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/GitCode/Caffe_Test/src/thirdparty/HDF5/hdf5-1.8.15-patch1/tools/lib
-# Build directory: E:/GitCode/Caffe_Test/src/thirdparty/HDF5/vs2013/tools/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
